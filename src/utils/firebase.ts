69b1396fab30ef84e79b1bc36a92ece947b2643b
rachid el aid <42470159+rachidelaid@users.noreply.github.com>
2023-05-13 10:26:57 +0100

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBi1B6qvLRTm_Kdqi36MW1px2q1oXb5VnA",
-  authDomain: "wordsgame-c3789.firebaseapp.com",
-  projectId: "wordsgame-c3789",
-  storageBucket: "wordsgame-c3789.appspot.com",
-  messagingSenderId: "797244131809",
-  appId: "1:797244131809:web:170e5edbf529a0b72e2aa0",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
